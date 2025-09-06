e655a6e8d1f67d40d79858abe77b979d24341aa5
Gayanga Kuruppu <50499511+cmdrGuyson@users.noreply.github.com>
2025-09-06 19:12:48 +0530

    feat: add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,28 @@ export const metadata: Metadata = {
     shortcut: "/logo.svg",
     apple: "/logo.svg",
   },
+  openGraph: {
+    title: "InkSink - The Kitchen Sink for Writing Content",
+    description: "The Kitchen Sink for Writing Content",
+    url: "https://inksink.com",
+    siteName: "InkSink",
+    images: [
+      {
+        url: "/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "InkSink - The Kitchen Sink for Writing Content",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "InkSink - The Kitchen Sink for Writing Content",
+    description: "The Kitchen Sink for Writing Content",
+    images: ["/og_image.png"],
+  },
 };
 
 export default function RootLayout({
