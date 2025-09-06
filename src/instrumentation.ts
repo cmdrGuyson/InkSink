76a75998718be05a9352087bc1882d8450745b75
Gayanga Kuruppu <50499511+cmdrGuyson@users.noreply.github.com>
2025-09-06 18:46:59 +0530

    Update instrumentation.ts

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,13 @@
+import * as Sentry from "@sentry/nextjs";
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
   registerOTel({ serviceName: "inksink" });
+
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 1.0,
+    });
+  }
 }
